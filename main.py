6a33b28e9a4868c5327c59adbee4b7d9a3c022e5
Nick Horton <nbh2@nist.gov>
2026-04-21 13:22:22 -0600

    current working version of T graph

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.constants import epsilon_0, mu_0
+
+db = lambda x: 20 * np.log10(np.abs(x))
 
 
 # Get the ABCD matrix of a transmission line
@@ -42,3 +46,68 @@ def abcd2s12_2(abcd, z, k_L, k_R, eta_L, eta_R, Gamma, d):
     t1 = (C + D / eta_L) * np.exp(-1j * k_L * z)
     t2 = Gamma * (C - D / eta_L) * np.exp(1j * k_L * z)
     return expl * (t1 + t2)
+
+
+f = 1e9
+num = 100
+Gammas = np.zeros(num, dtype=np.complex128)
+Ts = np.zeros(num, dtype=np.complex128)
+incident_angles = np.linspace(0, np.pi / 2, num)
+
+lambda_0 = 1 / (np.sqrt(epsilon_0 * mu_0) * f)
+print(lambda_0)
+
+epsrs = np.ones(12) * epsilon_0
+epsrs[1] = 2
+epsrs[-2] = 2
+epss =  epsrs * epsilon_0
+mus = np.ones(12) * mu_0
+ds = np.array(
+    [
+        0,
+        lambda_0 / 9,
+        lambda_0 / 9,
+        lambda_0 / 9,
+        lambda_0 / 9,
+        lambda_0 / 9,
+        lambda_0 / 9,
+        lambda_0 / 9,
+        lambda_0 / 9,
+        lambda_0 / 9,
+        lambda_0 / 9,
+        0,
+    ]
+)
+ks = 2.0 * np.pi * f * np.sqrt(mus * epss)
+kyz = 2.0 * np.pi * f * np.sqrt(mus[0] * epss[0]) * np.sin(incident_angles)
+for j, ang in enumerate(incident_angles):
+    thetas = np.zeros_like(ds, dtype=np.complex128)
+    thetas[0] = incident_angles[j]
+    for i in range(1, len(thetas)):
+        thetas[i] = np.arcsin(ks[i - 1] / ks[i] * np.sin(thetas[i - 1]))
+    etas_TM = np.sqrt(mus / epss) * np.cos(thetas)
+    kzs = ks * np.cos(thetas)
+
+    abcd_mats = []
+    for i in range(1, len(epss) - 1):
+        abcd_mats.append(tline_abcd(kzs[i], etas_TM[i], ds[i]))
+    abcd_mat = abcd_mats[0]
+    for i in range(1, len(abcd_mats)):
+        abcd_mat = abcd_mat @ abcd_mats[i]
+
+    Gamma = abcd2s11(abcd_mat, 0, kzs[0], etas_TM[0], etas_TM[-1])
+    T = abcd2s12_1(
+        abcd_mat, 0, kzs[0], kzs[-1], etas_TM[0], etas_TM[-1], Gamma, np.sum(ds)
+    )
+    Gammas[j] = Gamma
+    Ts[j] = T
+
+fig, ax = plt.subplots()
+print(Gammas[0], db(Gammas[0]))
+print(Ts[0], db(Ts[0]))
+# ax.plot(np.rad2deg(incident_angles), np.abs(Gammas))
+ax.plot(np.rad2deg(incident_angles), np.abs(Ts))
+
+greens_func = 1.0 + -(kyz**2) / (ks[0] ** 2)
+ax.plot(np.rad2deg(incident_angles), greens_func)
+plt.show()
--- test_main.py
+++ test_main.py
@@ -63,7 +63,7 @@ def test_multiple_tline_system_other_L_and_R():
     T2 = abcd2s12_2(M, 0, k_3, k_3, eta_3, eta_3, Gamma, d1 + d2)
 
     assert np.isclose(T1, T2)
-    assert np.isclose(np.abs(T1)**2 + np.abs(Gamma)**2, 1.0)
+    assert np.isclose(np.abs(T1) ** 2 + np.abs(Gamma) ** 2, 1.0)
     assert np.isclose(np.real(Gamma), -0.238, 0.01)
     assert np.isclose(np.imag(Gamma), -0.012, 0.02)
     assert np.isclose(db(Gamma), -12.445, 0.001)
@@ -74,9 +74,10 @@ def test_multiple_tline_system_other_L_and_R():
         print(T1, db(T1))
         print(np.abs(T1) ** 2 + np.abs(Gamma) ** 2)
 
+
 def test_tline_system_other_L_air_R():
     f = 1e9
-    
+
     eta_0 = np.sqrt(mu_0 / epsilon_0)
     eta_1 = np.sqrt(mu_0 / (2 * epsilon_0))
     eta_2 = np.sqrt(mu_0 / (3 * epsilon_0))
